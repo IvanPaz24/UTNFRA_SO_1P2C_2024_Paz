64eeedbdabfe6cf665f81fa28ad6fd38667fc6e3
IvanPaz24 <ivanpazaguero@gmail.com>
2024-10-06 20:56:36 -0400

    ADD: agregado y arreglo de A, B y C

--- RTA_SCRIPT_Examen_20241003/Punto_B.sh
+++ RTA_SCRIPT_Examen_20241003/Punto_B.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo fdisk /dev/sdb << EOF
+sudo fdisk /dev/sdd << EOF
 n
 p
 
@@ -42,34 +42,34 @@ n
 
 w
 EOF
-sudo mkfs -t ext4 /dev/sdb1
-sudo mkfs -t ext4 /dev/sdb2
-sudo mkfs -t ext4 /dev/sdb3
-sudo mkfs -t ext4 /dev/sdb5
-sudo mkfs -t ext4 /dev/sdb6
-sudo mkfs -t ext4 /dev/sdb7
-sudo mkfs -t ext4 /dev/sdb8
-sudo mkfs -t ext4 /dev/sdb9
-sudo mkfs -t ext4 /dev/sdb10
-sudo mkfs -t ext4 /dev/sdb11
-sudo mount /dev/sdb1 /Examenes-UTN/alumno_1/parcial_1
-sudo mount /dev/sdb2 /Examenes-UTN/alumno_1/parcial_2
-sudo mount /dev/sdb3 /Examenes-UTN/alumno_1/parcial_3
-sudo mount /dev/sdb5 /Examenes-UTN/alumno_2/parcial_1
-sudo mount /dev/sdb6 /Examenes-UTN/alumno_2/parcial_2
-sudo mount /dev/sdb7 /Examenes-UTN/alumno_2/parcial_3
-sudo mount /dev/sdb8 /Examenes-UTN/alumno_3/parcial_1
-sudo mount /dev/sdb9 /Examenes-UTN/alumno_3/parcial_2
-sudo mount /dev/sdb10 /Examenes-UTN/alumno_3/parcial_3
-sudo mount /dev/sdb11 /Examenes-UTN/profesores
-echo "/dev/sdb1 /Examenes-UTN/alumno_1/parcial_1 ext4 defaults 0 0" | sudo tee -a /etc/fstab
-echo "/dev/sdb2 /Examenes-UTN/alumno_1/parcial_2 ext4 defaults 0 0" | sudo tee -a /etc/fstab
-echo "/dev/sdb3 /Examenes-UTN/alumno_1/parcial_3 ext4 defaults 0 0" | sudo tee -a /etc/fstab
-echo "/dev/sdb5 /Examenes-UTN/alumno_2/parcial_1 ext4 defaults 0 0" | sudo tee -a /etc/fstab
-echo "/dev/sdb6 /Examenes-UTN/alumno_2/parcial_2 ext4 defaults 0 0" | sudo tee -a /etc/fstab
-echo "/dev/sdb7 /Examenes-UTN/alumno_2/parcial_3 ext4 defaults 0 0" | sudo tee -a /etc/fstab
-echo "/dev/sdb8 /Examenes-UTN/alumno_3/parcial_1 ext4 defaults 0 0" | sudo tee -a /etc/fstab
-echo "/dev/sdb9 /Examenes-UTN/alumno_3/parcial_2 ext4 defaults 0 0" | sudo tee -a /etc/fstab
-echo "/dev/sdb10 /Examenes-UTN/alumno_3/parcial_3 ext4 defaults 0 0" | sudo tee -a /etc/fstab
-echo "/dev/sdb11 /Examenes-UTN/profesores ext4 defaults 0 0" | sudo tee -a /etc/fstab
+sudo mkfs -t ext4 /dev/sdd1
+sudo mkfs -t ext4 /dev/sdd2
+sudo mkfs -t ext4 /dev/sdd3
+sudo mkfs -t ext4 /dev/sdd5
+sudo mkfs -t ext4 /dev/sdd6
+sudo mkfs -t ext4 /dev/sdd7
+sudo mkfs -t ext4 /dev/sdd8
+sudo mkfs -t ext4 /dev/sdd9
+sudo mkfs -t ext4 /dev/sdd10
+sudo mkfs -t ext4 /dev/sdd11
+sudo mount /dev/sdd1 Resolicion_A/Examenes-UTN/alumno_1/parcial_1
+sudo mount /dev/sdd2 Resolucion_A/Examenes-UTN/alumno_1/parcial_2
+sudo mount /dev/sdd3 Resolucion_A/Examenes-UTN/alumno_1/parcial_3
+sudo mount /dev/sdd5 Resolucion_A/Examenes-UTN/alumno_2/parcial_1
+sudo mount /dev/sdd6 Resolucion_A/Examenes-UTN/alumno_2/parcial_2
+sudo mount /dev/sdd7 Resolucion_A/Examenes-UTN/alumno_2/parcial_3
+sudo mount /dev/sdd8 Resolucion_A/Examenes-UTN/alumno_3/parcial_1
+sudo mount /dev/sdd9 Resolucion_A/Examenes-UTN/alumno_3/parcial_2
+sudo mount /dev/sdd10 Resolucion_A/Examenes-UTN/alumno_3/parcial_3
+sudo mount /dev/sdd11 Resolucion_A/Examenes-UTN/profesores
+echo "/dev/sdd1 Resolucion_A/Examenes-UTN/alumno_1/parcial_1 ext4 defaults 0 0" | sudo tee -a /etc/fstab
+echo "/dev/sdd2 Resolucion_A/Examenes-UTN/alumno_1/parcial_2 ext4 defaults 0 0" | sudo tee -a /etc/fstab
+echo "/dev/sdd3 Resolucion_A/Examenes-UTN/alumno_1/parcial_3 ext4 defaults 0 0" | sudo tee -a /etc/fstab
+echo "/dev/sdd5 Resolucion_A/Examenes-UTN/alumno_2/parcial_1 ext4 defaults 0 0" | sudo tee -a /etc/fstab
+echo "/dev/sdd6 Resolucion_A/Examenes-UTN/alumno_2/parcial_2 ext4 defaults 0 0" | sudo tee -a /etc/fstab
+echo "/dev/sdd7 Resolucion_A/Examenes-UTN/alumno_2/parcial_3 ext4 defaults 0 0" | sudo tee -a /etc/fstab
+echo "/dev/sdd8 Resolucion_A/Examenes-UTN/alumno_3/parcial_1 ext4 defaults 0 0" | sudo tee -a /etc/fstab
+echo "/dev/sdd9 Resolucion_A/Examenes-UTN/alumno_3/parcial_2 ext4 defaults 0 0" | sudo tee -a /etc/fstab
+echo "/dev/sdd10 Resolucion_A/Examenes-UTN/alumno_3/parcial_3 ext4 defaults 0 0" | sudo tee -a /etc/fstab
+echo "/dev/sdd11 Resolucion_A/Examenes-UTN/profesores ext4 defaults 0 0" | sudo tee -a /etc/fstab
 sudo mount -a
---
+++ RTA_SCRIPT_Examen_20241003/Punto_D.sh
@@ -0,0 +1,1 @@
+mkdir -p Resolucion_D/Estructura_Asimetrica/{correo,clientes}/cartas_{1..100} Resolucion_D/Estructura_Asimetrica/correo/carteros_{1..10}
